bd2126a86e083c00f48030aa23d9e2f3679927af
jenobpj <jenobgit@gmail.com>
2021-12-21 17:59:59 -0500

    add function to play the video

--- tiktok/src/Components/Video.js
+++ tiktok/src/Components/Video.js
@@ -1,10 +1,25 @@
 import "./video.css";
+import React, { useRef,useState } from "react";
 function Video() {
+  const videoRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+
+  const handleVideopress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
   return (
     <div className="video">
       <video
         className="video__player"
         loop
+        ref={videoRef}
+        onClick={handleVideopress}
         src="https://v16-webapp.tiktok.com/31f93a34a9d64318f2fe2691a16bc18c/61c273c3/video/tos/useast2a/tos-useast2a-ve-0068c004/e3eba515541e4fb58dab97d42f6fe331/?a=1988&br=3606&bt=1803&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFnkag3-I&l=202112211839210101901860440644A505&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzo7NGU6Zmp3OTMzNzczM0ApOjo3aTo5ZjxnNzxmO2c1Z2dfMDMtcjQwamlgLS1kMTZzczFjLi8wLy82Ll4uX14xXy86Yw%3D%3D&vl=&vr="
       ></video>
     </div>
