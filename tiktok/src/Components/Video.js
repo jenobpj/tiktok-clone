8406c16a2e0578897c04758dca2e111d85abe74f
jenobpj <jenobgit@gmail.com>
2021-12-21 17:38:58 -0500

    add snap functionality

--- tiktok/src/Components/Video.js
+++ tiktok/src/Components/Video.js
@@ -1,9 +1,13 @@
-import './video.css'
-function Video(){
-  return(
-    <div className='video'>
-     <video  className ="video__player" loop  src="https://v16-webapp.tiktok.com/6999aa92a49cdfc48663f41acf78cadc/61c2553e/video/tos/useast2a/tos-useast2a-ve-0068c004/b3c03c348c594fe7b7d651f240e3fe4f/?a=1988&br=3634&bt=1817&cd=0%7C0%7C0&ch=0&cr=0&cs=0&dr=0&ds=3&er=&ft=Yu12_FFnkag3-I&l=2021122116285801018905004121326A57&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amo6PDxsbjd4djMzZzczM0ApZTo1ZGc4ZGRlNzk7ZTpoNWdsZ2JzZHI1YTZfLS0wMTZzczZgXmI1XmMzNl5jMjMvYGI6Yw%3D%3D&vl=&vr=" ></video>
+import "./video.css";
+function Video() {
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        loop
+        src="https://v16-webapp.tiktok.com/31f93a34a9d64318f2fe2691a16bc18c/61c273c3/video/tos/useast2a/tos-useast2a-ve-0068c004/e3eba515541e4fb58dab97d42f6fe331/?a=1988&br=3606&bt=1803&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFnkag3-I&l=202112211839210101901860440644A505&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzo7NGU6Zmp3OTMzNzczM0ApOjo3aTo5ZjxnNzxmO2c1Z2dfMDMtcjQwamlgLS1kMTZzczFjLi8wLy82Ll4uX14xXy86Yw%3D%3D&vl=&vr="
+      ></video>
     </div>
-  )
+  );
 }
-export default Video
+export default Video;
