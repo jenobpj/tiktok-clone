3f6fc62e7396873c038c2566b6f2b25ba5e5c946
jenobpj <jenobgit@gmail.com>
2021-12-21 13:32:33 -0500

    hide the scrollbar and remove text in video components

--- tiktok/src/Components/Video.js
+++ tiktok/src/Components/Video.js
@@ -2,7 +2,6 @@ import './video.css'
 function Video(){
   return(
     <div className='video'>
-      <h1>jenob</h1>
      <video  className ="video__player" loop  src="https://v16-webapp.tiktok.com/6999aa92a49cdfc48663f41acf78cadc/61c2553e/video/tos/useast2a/tos-useast2a-ve-0068c004/b3c03c348c594fe7b7d651f240e3fe4f/?a=1988&br=3634&bt=1817&cd=0%7C0%7C0&ch=0&cr=0&cs=0&dr=0&ds=3&er=&ft=Yu12_FFnkag3-I&l=2021122116285801018905004121326A57&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amo6PDxsbjd4djMzZzczM0ApZTo1ZGc4ZGRlNzk7ZTpoNWdsZ2JzZHI1YTZfLS0wMTZzczZgXmI1XmMzNl5jMjMvYGI6Yw%3D%3D&vl=&vr=" ></video>
     </div>
   )
