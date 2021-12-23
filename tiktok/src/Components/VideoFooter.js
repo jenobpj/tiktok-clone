8aa7dec6ed46a57fedbe045ba77a1ec19271c627
jenobpj <jenobgit@gmail.com>
2021-12-23 18:45:06 -0500

    Install material UI and icons

--- tiktok/src/Components/VideoFooter.js
+++ tiktok/src/Components/VideoFooter.js
@@ -1,13 +1,18 @@
 import React from "react";
-import './VideoFooter.css'
-function VideoFooter (){
-  return(
+import "./VideoFooter.css";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+function VideoFooter() {
+  return (
     <div className="videoFooter">
-       <div className="videoFooter__text">
-       <h3>@jenob</h3>
-       <p>This is some description</p>
-       </div>
+      <div className="videoFooter__text">
+        <h3>@jenob</h3>
+        <p>This is some description</p>
+
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 export default VideoFooter;
