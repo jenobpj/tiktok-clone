46a99fe30e956f07ce97628796526a475d54749a
jenobpj <jenobgit@gmail.com>
2021-12-26 23:42:56 -0500

    import favourite icons

--- tiktok/src/Components/VideoSidebar.js
+++ tiktok/src/Components/VideoSidebar.js
@@ -1,7 +1,22 @@
 import "./Videosidebar.css";
+import FavoriteIcon from "@material-ui/icons/Favorite"
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import MessageIcon from '@material-ui/icons/Message';
+import ShareIcon from '@material-ui/icons/Share';
 function videoSidebar() {
   return <div className="videoSidebar">
-     <h1>Iam side bar</h1>
+     <div className="videoSidebar__button">
+       <FavoriteBorderIcon/>
+       <p>200</p>
+     </div>
+     <div className="videoSidebar__button">
+       <MessageIcon/>
+       <p>300</p>
+     </div>
+     <div className="videoSidebar__button">
+       <ShareIcon/>
+       <p>400</p>
+     </div>
   </div>;
 }
 export default videoSidebar;
