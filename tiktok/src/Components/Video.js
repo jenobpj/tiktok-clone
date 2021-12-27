47d288a47884d2c8ce92d101a7e850719d91820a
jenobpj <jenobgit@gmail.com>
2021-12-27 13:27:57 -0500

    add git like button functionality

--- tiktok/src/Components/Video.js
+++ tiktok/src/Components/Video.js
@@ -22,7 +22,7 @@ function Video() {
         loop
         ref={videoRef}
         onClick={handleVideopress}
-        src="https://v16-webapp.tiktok.com/3ce98cf3d80f9e277974bc4faae91ee2/61c97513/video/tos/useast2a/tos-useast2a-pve-0068/fa4d3d94aa8d46b994e97a046e09930a/?a=1988&br=3160&bt=1580&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFnkag3-I&l=2021122702105001022308301222BAE5E0&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anc2djg6ZnduOjMzNzczM0ApZ2Y1OzNoOzw5NzY8OWdpOWdrZmtmcjQwMDBgLS1kMTZzc2EwYmE0MC9iMzY2LzBiLy46Yw%3D%3D&vl=&vr="
+        src="https://v16-webapp.tiktok.com/23880cabfbe060e69e9cc926d093ac87/61ca56a2/video/tos/useast2a/tos-useast2a-ve-0068c003/34539573e47449b19885c94732c51a58/?a=1988&br=2550&bt=1275&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFnkag3-I&l=202112271813130102230650520C274EE8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amdkbjw6ZnBsOTMzNzczM0ApaGk7Ojw0Nzs4N2Y3OmQ6N2dzYV4wcjRfaWZgLS1kMTZzc2AuMl5jY14vLy9hLjNgYjU6Yw%3D%3D&vl=&vr=git"
 ></video>
  <VideoFooter channel='jenob' descriptiion="it is cool" song="cool ka bap"/>
  <VideoSidebar/>
--- tiktok/src/Components/VideoSidebar.js
+++ tiktok/src/Components/VideoSidebar.js
@@ -1,22 +1,26 @@
 import "./Videosidebar.css";
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import MessageIcon from '@material-ui/icons/Message';
-import ShareIcon from '@material-ui/icons/Share';
-function videoSidebar() {
-  return <div className="videoSidebar">
-     <div className="videoSidebar__button">
-       <FavoriteBorderIcon/>
-       <p>200</p>
-     </div>
-     <div className="videoSidebar__button">
-       <MessageIcon/>
-       <p>300</p>
-     </div>
-     <div className="videoSidebar__button">
-       <ShareIcon/>
-       <p>400</p>
-     </div>
-  </div>;
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
+import { useState } from "react";
+function VideoSidebar() {
+  const [liked, setLiked] = useState(false);
+  return (
+    <div className="videoSidebar">
+      <div className="videoSidebar__button">
+        {liked ? <FavoriteIcon  onClick={(e)=> setLiked(false)}/> : <FavoriteBorderIcon onClick={(e)=>setLiked(true)} />}
+        <p>200</p>
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon />
+        <p>300</p>
+      </div>
+      <div className="videoSidebar__button">
+        <ShareIcon />
+        <p>400</p>
+      </div>
+    </div>
+  );
 }
-export default videoSidebar;
+export default VideoSidebar;
