f78424fb6a6ba520c54fc5f6711547054d973bbe
jenobpj <jenobgit@gmail.com>
2021-12-28 14:20:13 -0500

    add post end point to insert videos

--- tiktok_backend/index.js
+++ tiktok_backend/index.js
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 //app config
 const app = express();
 const port = 9000;
+//middleware
+app.use(express.json());
 
 const connection_url =
   "mongodb+srv://admin:6h6g6z23SGyDjDF@cluster0.8mog6.mongodb.net/tiktok?retryWrites=true&w=majority";
@@ -17,6 +19,17 @@ mongoose.connect(connection_url, {
 app.get("/", (req, res) => {
   res.status(200).send("hello world");
 });
+app.post('/v2/posts',(req,res)=>{
+  const dbVideos=req.body;
+
+  Videos.create(dbVideos,(err,data)=>{
+    if(err){
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`listening on localhose ${port}`);
